7a8249095c8ab95b156fc8d81efbe5adc338180d
AaronTheTitan <abeats20@gmail.com>
2016-09-11 01:59:25 -0500

    add nav search support

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">About</h1>
-      <p>This is a weather application built on React. I've built this for fun.</p>
+      <p>This is a weather application built on React. I\'ve built this for fun.</p>
         <p>
             Here are some of the tools used:
         </p>
